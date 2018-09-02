755d32c8d5e9831370adf28a44b387d73f839c38
Kevin Harrington <mad.hephaestus@gmail.com>
2018-09-02 18:25:16 -0400

    Bugfix of concurrent modification of polling queue (#6)
    * Delete SimplePacketComsJavaFat-0.5.2.jar
    
    * Updaing jar
    
    * Using and testing new command
    
    * Using and testing new command
    
    * Adding blocking to the startup of commands
    
    * Adding blocking to the startup of commands
    
    * Removing junk command send

--- src/lab1.m
+++ src/lab1.m
@@ -17,7 +17,7 @@ vid = hex2dec('3742');
 pid = hex2dec('0007');
 disp (vid );
 disp (pid);
-javaaddpath ../lib/SimplePacketComsJavaFat-0.5.2.jar;
+javaaddpath ../lib/SimplePacketComsJavaFat-0.6.3.jar;
 import edu.wpi.SimplePacketComs.*;
 import edu.wpi.SimplePacketComs.device.*;
 import edu.wpi.SimplePacketComs.phy.*;
@@ -59,7 +59,7 @@ try
       % Send packet to the server and get the response
       returnPacket = pp.command(SERV_ID, packet);
       
-
+      
       if DEBUG
           disp('Sent Packet:');
           disp(packet);
