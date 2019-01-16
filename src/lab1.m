bed6c4da6b8ec15154aaba48073698378b55e79a
Loris Fichera <loris.fichera@gmail.com>
2019-01-16 08:14:47 -0500

    Update lab1.m (#11)

--- src/lab1.m
+++ src/lab1.m
@@ -10,14 +10,19 @@
 % 
 % IMPORTANT - understanding the code below requires being familiar
 % with the Nucleo firmware. Read that code first.
+
+% Lines 15-37 perform necessary library initializations. You can skip reading
+% to line 38.
 clear
 clear java
-%clear import;
 clear classes;
+
 vid = hex2dec('3742');
 pid = hex2dec('0007');
-disp (vid );
+
+disp (vid);
 disp (pid);
+
 javaaddpath ../lib/SimplePacketComsJavaFat-0.6.4.jar;
 import edu.wpi.SimplePacketComs.*;
 import edu.wpi.SimplePacketComs.device.*;
@@ -29,10 +34,11 @@ myHIDSimplePacketComs=HIDfactory.get();
 myHIDSimplePacketComs.setPid(pid);
 myHIDSimplePacketComs.setVid(vid);
 myHIDSimplePacketComs.connect();
+
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = PacketProcessor(myHIDSimplePacketComs); 
 try
-  SERV_ID = 01;            % we will be talking to server ID 37 on
+  SERV_ID = 01;            % we will be talking to server ID 01 on
                            % the Nucleo
 
   DEBUG   = true;          % enables/disables debug prints
@@ -47,18 +53,12 @@ try
   % setpoints to the Nucleo firmware. 
   viaPts = [0, -400, 400, -400, 400, 0];
 
-  
-
-  % Iterate through a sine wave for joint values
   for k = viaPts
       tic
-      %incremtal = (single(k) / sinWaveInc);
       packet = zeros(15, 1, 'single');
       packet(1) = k;
 
-     
-      % Send packet to the server and get the response
-      
+      % Send packet to the server and get the response      
       %pp.write sends a 15 float packet to the micro controller
        pp.write(SERV_ID, packet); 
        
@@ -80,27 +80,18 @@ try
           packet((x*3)+2)=0;
           packet((x*3)+3)=0;
       end
-      %THis version will send the command once per call of pp.write
-      pp.write(02, packet);
-      pause(0.003);
-      returnPacket2=  pp.read(02);
-      %this version will start an auto-polling server and read back the
-      %current data
-      %returnPacket2=  pp.command(65, packet);
-      if DEBUG
-          disp('Received Packet 2:');
-          disp(returnPacket2);
-      end
+      
       toc
       pause(1) %timeit(returnPacket) !FIXME why is this needed?
       
   end
+  
 catch exception
     getReport(exception)
     disp('Exited on error, clean shutdown');
 end
+
 % Clear up memory upon termination
 pp.shutdown()
-
 
 toc
