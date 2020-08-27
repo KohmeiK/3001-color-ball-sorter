0f253aed71e148fd00516420b92c01285d24d9d1
Kevin Harrington <mad.hephaestus@gmail.com>
2020-08-27 17:39:02 -0400

    comments

--- src/lab1.m
+++ src/lab1.m
@@ -38,7 +38,7 @@ myHIDSimplePacketComs.connect();
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = Robot(myHIDSimplePacketComs); 
 try
-  SERV_ID = 1848;            % we will be talking to server ID 01 on
+  SERV_ID = 1848;            % we will be talking to server ID 1848 on
                            % the Nucleo
   SERVER_ID_READ =1910;% ID of the read packet
   DEBUG   = true;          % enables/disables debug prints
