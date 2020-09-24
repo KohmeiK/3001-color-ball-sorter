baa98020acd181dcbc197c4a9640b670a019daaa
Jason Martino <jasonamartino@gmail.com>
2020-09-24 00:55:52 -0400

    Moved coeff calculation into constructor and nearly finished interpolator class rev 1

--- src/Interpolator.m
+++ src/Interpolator.m
@@ -3,8 +3,7 @@ classdef Interpolator
     %   Detailed explanation goes here
 
     properties
-        coeffs3 = zeros(4,3);
-        coeffs5 = zeros(6,3);
+        coeffs;
         type;
         T;
     end
@@ -15,40 +14,69 @@ classdef Interpolator
             %   Detailed explanation goes here
             obj.type = InterpolationType;
             obj.T = Time;
+            
+            t0 = 0;
+            tf = obj.T;
+            v0 = 0;
+            vf = 0;
+            q0 = 0;
+            qf = 1;
+            alpha0 = 0;
+            alphaf = 0;
+            
+            if InterpolationType == "Cubic"
+                MCubic = [1 t0  t0^2    t0^3;
+                          0 1   2*t0    3*t0^2;
+                          1 tf  tf^2    tf^3;
+                          0 1   2*tf    3*tf^2];
+                     
+                AnsCubic = [q0;
+                            v0;
+                            qf;
+                            vf];
+
+                tempCubic = linsolve(MCubic,AnsCubic);
+                obj.coeffs(:,3) = tempCubic;
+            
+            elseif InterpolationType == "Quintic"
+                MQuintic = [1   t0  t0^2    t0^3    t0^4        t0^5;
+                            0   1   2*t0    3*0^2   4*t0^3      5*t0^4;
+                            0   0   2       6*t0    12*t0^2     20*t0^3;
+                            1   tf  tf^2    tf^3    tf^4        tf^5;
+                            0   1   2*tf    3*tf^2  4*tf^3      5*tf^4;
+                            0   0   2       6*tf    12*tf^2     20*tf^3];
+                        
+                AnsQuintic = [q0;
+                              v0;
+                              alpha0;
+                              qf;
+                              vf;
+                              alphaf];
+                          
+                tempQuintic = linSolve(MQuintic,AnsQuintic);
+                obj.coeffs(:,5) = tempQuintic;
+            end
+            
         end
         
         function Scalar = get(deltaT)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
+            A = obj.coeffs;
             
-        t0 = 0;
-        tf = Interpolator.T;
-        v0 = 0;
-        vf = 0;
-        q0 = 0;
-        qf = 1;
-        
-            if Interpolator.type == "Linear"
-               Scalar = (deltaT/Interpolator.T);
+            if obj.type == "Linear"
+               Scalar = (deltaT/obj.T);
            
-            elseif Interpolator.type == "Cubic"
-                M = [a(0) a(1)*t0 a(2)*t0^2 a(3)*t0^3
-                    a(0) a(1)*tf a(2)*tf^2 a(3)*tf^3
-                    a(1) 2*a(2)*t0 3*a(3)*t0^2
-                    a(1) 2*a(2)*tf 3*a(3)*tf^2];
-                    
-                Ans = [q0 
-                      qf 
-                      v0 
-                      vf];
+            elseif obj.type == "Cubic"
+              
+                Scalar = A(0) + A(1)*deltaT + A(2)*deltaT^2 + A(3)*deltaT*3;
                 
-                temp = linsolve(M,Ans);
-                Interpolator.coeffs3(:,index) = temp;
-                Scalar = Inerpolator.coeffs3;
+            elseif obj.type == "Quintic"
                 
-            elseif Interpolator.type == "Quintic"
                 Scalar = 0;
+                
             end
         end
+        
     end
 end
