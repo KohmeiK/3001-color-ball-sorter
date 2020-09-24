5733829c122804011ac813c3915afbf24abdaf64
Jason Martino <jasonamartino@gmail.com>
2020-09-24 01:18:17 -0400

    Interpolator class rev 1 finished, need to add loop to main class tomorrow

--- src/Interpolator.m
+++ src/Interpolator.m
@@ -5,7 +5,7 @@ classdef Interpolator
     properties
         coeffs;
         type;
-        T;
+        T = 0;
     end
 
     methods
@@ -15,6 +15,7 @@ classdef Interpolator
             obj.type = InterpolationType;
             obj.T = Time;
             
+            %Set Known Parameters
             t0 = 0;
             tf = obj.T;
             v0 = 0;
@@ -24,6 +25,7 @@ classdef Interpolator
             alpha0 = 0;
             alphaf = 0;
             
+            %Calculate Coefficients for Cubic Trajectory and store in obj.coeffs
             if InterpolationType == "Cubic"
                 MCubic = [1 t0  t0^2    t0^3;
                           0 1   2*t0    3*t0^2;
@@ -38,13 +40,15 @@ classdef Interpolator
                 tempCubic = linsolve(MCubic,AnsCubic);
                 obj.coeffs(:,3) = tempCubic;
             
+            %Calculate Coefficients for Quintic Trajectory and store in obj.coeffs
             elseif InterpolationType == "Quintic"
                 MQuintic = [1   t0  t0^2    t0^3    t0^4        t0^5;
                             0   1   2*t0    3*0^2   4*t0^3      5*t0^4;
                             0   0   2       6*t0    12*t0^2     20*t0^3;
                             1   tf  tf^2    tf^3    tf^4        tf^5;
                             0   1   2*tf    3*tf^2  4*tf^3      5*tf^4;
-                            0   0   2       6*tf    12*tf^2     20*tf^3];
+                            0   0   2       6*tf    12*tf^2     20*tf^3];    
+                        
                         
                 AnsQuintic = [q0;
                               v0;
@@ -73,8 +77,8 @@ classdef Interpolator
                 
             elseif obj.type == "Quintic"
                 
-                Scalar = 0;
-                
+                Scalar = A(0) + A(1)*deltaT + A(2)*deltaT^2 + A(3)*deltaT*3 + ...
+                    A(4)*deltaT*4 + A(5)*deltaT*5;
             end
         end
         
